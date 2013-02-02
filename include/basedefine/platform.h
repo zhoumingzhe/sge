d30448499c98481638983e2098ca3357a54eb127
Mingzhe Zhou <zhoumingzhe@gmail.com>
2013-02-02 18:36:25 +0800

    Types

---
+++ include/basedefine/platform.h
@@ -0,0 +1,45 @@
+#ifndef BASEDEFINE_MACROS_H
+#define BASEDEFINE_MACROS_H
+
+#if defined (__GNUC__)
+#define SGE_GCC
+#elif defined _MSC_VER
+#define SGE_MSVC
+#endif
+
+#ifdef SGE_GCC
+#if defined __amd64__
+#define SGE_64
+#define SGE_INTEL
+#elif defined __i386__
+#define SGE_32
+#define SGE_INTEL
+#endif
+#endif
+
+#ifdef SGE_MSVC
+#if defined (_M_AMD64) || defined (_M_X64)
+#define SGE_64
+#define SGE_INTEL
+#elif defined (_M_IX86)
+#define SGE_32
+#define SGE_INTEL
+#endif
+#endif
+
+#if defined SGE_GCC
+#define sge_align(aligntype) __attribute__((aligned(aligntype)))
+#define sge_export
+#define sge_import
+#define sge_inline static __inline __attribute__((always_inline))
+#define sge_thread_local __thread
+#elif defined SGE_MSVC
+#define sge_align(aligntype) __declspec(align(aligntype))
+#define sge_export __declspec(dllexport)
+#define sge_import __declspec(dllimport)
+#define sge_inline __forceinline
+#define sge_thread_local __declspec(thread)
+#endif
+
+#endif
+
---
+++ include/basedefine/types.h
@@ -0,0 +1,20 @@
+#ifndef BASEDEFINE_TYPES_H
+#define BASEDEFINE_TYPES_H
+#include "platform.h"
+
+//unsigned builtin types
+typedef unsigned char sge_uint8;
+typedef unsigned short sge_uint16;
+typedef unsigned int sge_uint32;
+typedef unsigned long long sge_uint64;
+
+//signed builtin types
+typedef char sge_int8;
+typedef short sge_int16;
+typedef int sge_int32;
+typedef long long sge_int64;
+
+typedef float float32;
+typedef double float64;
+
+#endif
