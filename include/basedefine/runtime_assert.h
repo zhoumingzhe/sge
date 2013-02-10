61b729a909c23bc12cebfcd03e84dfaa6ffdd2d5
Mingzhe Zhou <zhoumingzhe@gmail.com>
2013-02-10 20:59:13 +0800

    customizable runtime output function

--- include/basedefine/runtime_assert.h
+++ include/basedefine/runtime_assert.h
@@ -2,6 +2,8 @@
 #define BASEDEFINE_RUNTIME_ASSERT_H
 #include "platform.h"
 SGE_EXTERN_C_START
+typedef int (*sge_runtime_assert_output_func)(const char *format,...);
+sge_runtime_assert_output_func sge_set_runtime_assert_output(sge_runtime_assert_output_func);
 void _sge_runtime_assert(int expr, const char* msg, int line, const char* file);
 SGE_EXTERN_C_END
 #define sge_runtime_assert(expr) _sge_runtime_assert(expr, #expr, __LINE__, __FILE__)
--- src/basedefine/runtime_assert.c
+++ src/basedefine/runtime_assert.c
@@ -13,8 +13,14 @@ static int default_runtime_assert_output(const char *format,...)
     return ret;
 }
 
-static int (*runtime_assert_output)(const char *format,...) = default_runtime_assert_output;
+sge_runtime_assert_output_func runtime_assert_output = default_runtime_assert_output;
 
+sge_runtime_assert_output_func sge_set_runtime_assert_output(sge_runtime_assert_output_func f)
+{
+    sge_runtime_assert_output_func old = runtime_assert_output;
+    runtime_assert_output = f;
+    return old;
+}
 
 void _sge_runtime_assert(int expr, const char* msg, int line, const char* file)
 {
