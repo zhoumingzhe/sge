556fa6664db3f6c4861b22a8192a6422d888f1d7
Mingzhe Zhou <zhoumingzhe@gmail.com>
2013-03-09 21:19:20 +0800

    to lua structure test

--- test/lua/tolua/interface_c.c
+++ test/lua/tolua/interface_c.c
@@ -1,3 +1,4 @@
+#include "interface_c.h"
 int add(int x, int y)
 {
 return x + y;
@@ -5,5 +6,10 @@ return x + y;
 int sub(int x, int y)
 {
 return x - y;
+}
+
+test_type getresult(test_type v)
+{
+    return v;
 }
 
--- test/lua/tolua/interface_c.h
+++ test/lua/tolua/interface_c.h
@@ -1,3 +1,9 @@
 
+typedef struct test_type
+{
+    int x;
+    int y;
+}test_type;
+test_type getresult(test_type v);
 int add(int x, int y);
 int sub(int x, int y);
