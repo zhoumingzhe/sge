5a587391f86893be4826af1274e31a59ec08678d
Mingzhe Zhou <zhoumingzhe@gmail.com>
2013-05-25 14:58:34 +0800

    vector ceiling and floor

--- include/mathlib/vector.h
+++ include/mathlib/vector.h
@@ -1,20 +1,46 @@
+#ifndef MATHLIB_VECTOR_H
+#define MATHLIB_VECTOR_H
 #include "basedefine/types.h"
 #include <xmmintrin.h>
+#include <emmintrin.h>
 
+//-----------------------------typedefs--------------------
 typedef __m128 sge_vec4f;
-typedef sge_float32 sge_vec4f_unalign[4];
+typedef __m128i sge_vec4i;
+typedef __m128d sge_vec2d;
+
+typedef sge_float32 sge_vec4f_unaligned[4];
+typedef sge_int32 sge_vec4i_unaligned[4];
+typedef sge_float64 sge_vec2d_unaligned[2];
+
+typedef sge_align(16) sge_float32 sge_vec4f_aligned[4];
+typedef sge_align(16) sge_int32 sge_vec4i_aligned[4];
+typedef sge_align(16) sge_float64 sge_vec2d_aligned[2];
+
+//--------------------------constants---------------------
+
+extern const sge_vec4f_aligned sge_zero;
+extern const sge_vec4f_aligned sge_no_fraction;
+
+
+
+extern const sge_vec4i_aligned sge_onehalf_eps;
+extern const sge_vec4i_aligned sge_abs_mask;
 
 //sse shuffle macro, internal use only
 #define sge_sse_shuffle(f0, f1, f2, f3)\
     (((f3) << 6) | ((f2) << 4) | ((f1) << 2) | ((f0)))
 
-//initialization functions
+//---------------initialization functions-----------------
 
 #define sge_vec4f_zero() _mm_setzero_ps()
 #define sge_vec4f_rep(val) _mm_set_ps1(val)
 #define sge_vec4f_init(x, y, z, w) _mm_set_ps(w, z, y, x)
+#define sge_vec4f_load(addr) _mm_load_ps(addr)
 
-//--------------access functions-------------------
+#define sge_vec4i_load(addr) _mm_load_si128((sge_vec4i*)addr)
+
+//--------------------access functions-------------------
 
 #define sge_vec4f_getx(val) _mm_cvtss_f32(val)
 sge_inline float sge_vec4f_gety(sge_vec4f val)
@@ -85,7 +111,7 @@ sge_inline sge_vec4f sge_vec4f_scale(sge_vec4f val, sge_float32 scale)
     return sge_vec4f_mul(val, temp);
 }
 
-sge_inline sge_vec4f sge_vec4_abs(sge_vec4f val)
+sge_inline sge_vec4f sge_vec4f_abs(sge_vec4f val)
 {
     sge_vec4f temp = sge_vec4f_zero();
     temp = sge_vec4f_sub(temp, val);
@@ -99,8 +125,33 @@ sge_inline sge_vec4f sge_vec4f_clamp(sge_vec4f val, sge_vec4f minval, sge_vec4f 
 }
 
 //todo
-sge_vec4f sge_vec4f_floor(sge_vec4f val);
-sge_vec4f sge_vec4f_celling(sge_vec4f val);
+sge_inline sge_vec4f sge_vec4f_floor(sge_vec4f val)
+{
+    sge_vec4f result = _mm_sub_ps(val, _mm_castsi128_ps(sge_vec4i_load(sge_onehalf_eps)));
+    sge_vec4i vInt = _mm_cvtps_epi32(result);
+    sge_vec4i abs_int = _mm_and_si128(_mm_castps_si128(val), sge_vec4i_load(sge_abs_mask));
+
+    abs_int = _mm_cmplt_epi32(abs_int, sge_vec4i_load(sge_no_fraction));
+    result = _mm_cvtepi32_ps(vInt);
+    result = _mm_and_ps(result, _mm_castsi128_ps(abs_int));
+    abs_int = _mm_andnot_si128(abs_int, _mm_castps_si128(val));
+    result = _mm_or_ps(result, _mm_castsi128_ps(abs_int));
+    return result;
+}
+
+sge_inline sge_vec4f sge_vec4f_celling(sge_vec4f val)
+{
+    sge_vec4f result = _mm_add_ps(val, _mm_castsi128_ps(sge_vec4i_load(sge_onehalf_eps)));
+    sge_vec4i vInt = _mm_cvtps_epi32(result);
+    sge_vec4i abs_int = _mm_and_si128(_mm_castps_si128(val), sge_vec4i_load(sge_abs_mask));
+
+    abs_int = _mm_cmplt_epi32(abs_int, sge_vec4i_load(sge_no_fraction));
+    result = _mm_cvtepi32_ps(vInt);
+    result = _mm_and_ps(result, _mm_castsi128_ps(abs_int));
+    abs_int = _mm_andnot_si128(abs_int, _mm_castps_si128(val));
+    result = _mm_or_ps(result, _mm_castsi128_ps(abs_int));
+    return result;
+}
 
 //--------------comparison functions-------------------
 sge_inline sge_bool sge_vec4f_eql(sge_vec4f val1, sge_vec4f val2)
@@ -135,3 +186,19 @@ sge_inline sge_bool sge_vec4f_eqlw(sge_vec4f val, float w)
     sge_vec4f temp1 = _mm_shuffle_ps(val, val, sge_sse_shuffle(3, 3, 3, 3));
     return (_mm_ucomieq_ss(temp, temp1));
 }
+
+sge_inline sge_bool sge_vec4f_lt(sge_vec4f val1, sge_vec4f val2)
+{
+    sge_vec4f temp = _mm_cmplt_ps(val1,val2);
+    return (_mm_movemask_ps(temp)==0x0f);
+}
+
+sge_inline sge_bool sge_vec4f_near_eql(sge_vec4f val1, sge_vec4f val2, sge_vec4f tolerance)
+{
+    sge_vec4f delta = sge_vec4f_sub(val1, val2);
+    delta = sge_vec4f_abs(delta);
+    return sge_vec4f_lt(delta, tolerance);
+}
+
+
+#endif
--- src/mathlib/vector.c
+++ src/mathlib/vector.c
@@ -1,1 +1,10 @@
 #include "vector.h"
+
+const sge_vec4f_aligned sge_zero = {0.0f, 0.0f, 0.0f, 0.0f};
+const sge_vec4f_aligned sge_no_fraction = {8388608.0f,8388608.0f,8388608.0f,8388608.0f};
+
+
+const sge_vec4i_aligned sge_abs_mask = {0x7FFFFFFF, 0x7FFFFFFF, 0x7FFFFFFF, 0x7FFFFFFF};
+const sge_vec4i_aligned sge_onehalf_eps= { 0x3EFFFFFD, 0x3EFFFFFD, 0x3EFFFFFD, 0x3EFFFFFD};
+
+
