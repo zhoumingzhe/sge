6f197fcb132ccfd6e4cd0256059c6af7c783e635
Mingzhe Zhou <zhoumingzhe@gmail.com>
2013-06-05 23:51:59 +0800

    matrix translate

--- include/mathlib/matrix.h
+++ include/mathlib/matrix.h
@@ -11,7 +11,7 @@ typedef sge_align(16) union sge_mat44f
         float _31, _32, _33, _34;
         float _41, _42, _43, _44;
     };
-    float m[4][4];
+    sge_float32 m[4][4];
 }sge_mat44f;
 
 sge_mat44f sge_mat44f_identity()
@@ -23,4 +23,15 @@ sge_mat44f sge_mat44f_identity()
     ret.r[3] = sge_vec4f_load_aligned(sge_vec4f_0001);
     return ret;
 }
+
+sge_mat44f sge_mat44f_translate(sge_float32 x, sge_float32 y, sge_float32 z)
+{
+    sge_mat44f ret;
+    ret.r[0] = sge_vec4f_load_aligned(sge_vec4f_1000);
+    ret.r[1] = sge_vec4f_load_aligned(sge_vec4f_0100);
+    ret.r[2] = sge_vec4f_load_aligned(sge_vec4f_0010);
+    ret.r[3] = sge_vec4f_init(x, y, z, 1.f);
+    return ret;
+}
+
 #endif
