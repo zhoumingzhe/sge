b3e4b6d6000c20f75b9fe00e16bcd02c7690980d
Mingzhe Zhou <zhoumingzhe@gmail.com>
2013-07-21 16:37:47 +0800

    aligned memory allocator

--- include/basedefine/utility.h
+++ include/basedefine/utility.h
@@ -4,4 +4,9 @@
 #define get_container(address, type, field)\
     (type*)((sge_uint8*)(address)-(sge_uint8*)(&((type*)0)->field))
 
+#define align_down(len, alignment)\
+    ((len) & ~(alignment - 1))
+#define align_up(len, alignment)\
+    ((len + alignment - 1)&~(alignment - 1))
+
 #endif
--- src/memory/memory.c
+++ src/memory/memory.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
 #include "sge_memory.h"
+#include "basedefine/utility.h"
+#define SGE_MALLOC_ALIGN 16
 void* sge_malloc(sge_size_t size)
 {
-    return malloc(size);
+    void* p = malloc(size + SGE_MALLOC_ALIGN);
+    void* pret = (void*)align_up((sge_size_t)p+1, SGE_MALLOC_ALIGN);
+    *((sge_uint8*)pret-1) = (sge_uint8)((sge_uint8*)pret - (sge_uint8*)p);
+    return pret;
 }
 void sge_free(void* mem)
 {
-    free(mem);
+    void* p = (sge_uint8*)mem - *((sge_uint8*)mem-1);
+    free(p);
 }
 
